32903a218e8de5bd25b706a2479ffb92a8f71350
SakuraSher <sakurasherlocked@gmail.com>
2026-08-05 19:06:44 +0530

    added recovery_window_in_days option in db secret

--- ver2/infra/secrets.tf
+++ ver2/infra/secrets.tf
@@ -1,6 +1,7 @@
 resource "aws_secretsmanager_secret" "db_secret"{
-    name = "db-env-string2"
+    name = "db-env-secret"
     description = "db-env-string"
+    recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret"{
